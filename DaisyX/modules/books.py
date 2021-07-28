2e9239a7e7e77dd707493665ec1c21064535436c
KL10Movies <65135315+muhssiin@users.noreply.github.com>
2021-07-28 20:58:18 +0530

    Update books.py

--- DaisyX/modules/books.py
+++ DaisyX/modules/books.py
@@ -65,14 +65,14 @@ async def _(event):
                 f.write("\n" + title)
                 f.write("\nBook link:- " + link + "\n\n")
 
-        f.write("By @DaisyXBot.")
+        f.write("By @MCubeManagerBot.")
         f.close()
-        caption = "A collabration with Friday.\n Join Support @DaisySupport_Official"
+        caption = "A collabration with Friday.\n Join Support @MCubeMediaOfficial"
 
         await tbot.send_file(
             event.chat_id,
             "book.txt",
-            caption=f"**BOOKS GATHERED SUCCESSFULLY!\n\nBY DAISYX. JOIN THE SUPPORT @DaisySupport_Official.**",
+            caption=f"**BOOKS GATHERED SUCCESSFULLY!\n\nBY DAISYX. JOIN THE SUPPORT @MCubeMediaOfficial.**",
         )
         os.remove("book.txt")
         await KkK.delete()
