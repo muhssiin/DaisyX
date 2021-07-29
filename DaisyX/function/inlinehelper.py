274082957fa705f0a16de9df37aa9d95e186b8a0
KL10Movies <65135315+muhssiin@users.noreply.github.com>
2021-07-29 17:58:52 +0530

    Update inlinehelper.py

--- DaisyX/function/inlinehelper.py
+++ DaisyX/function/inlinehelper.py
@@ -101,7 +101,7 @@ async def alive_function(answers):
         InlineQueryResultArticle(
             title="Alive",
             description="Check Bot's Stats",
-            thumb_url="https://telegra.ph/file/debc179305d2e1f140636.jpg",
+            thumb_url="https://telegra.ph/file/91b8f4a7d59cac868d576.jpg",
             input_message_content=InputTextMessageContent(
                 msg, disable_web_page_preview=True
             ),
